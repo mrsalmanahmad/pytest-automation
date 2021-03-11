61a6ae3082528d745b78426e199070f346b31df7
salmanahmad92 <salmanahmad3333@gmail.com>
2021-03-11 13:28:31 +0500

    a

--- test/browser_setup.py
+++ test/browser_setup.py
@@ -5,9 +5,10 @@ from selenium.webdriver.common.keys import Keys
 @pytest.fixture
 def browser():
   # use this in-case your env path of chromedriver is not setup  
-  #driver = webdriver.Chrome('/path/to/chromedriver') 
+  driver = webdriver.Chrome('C:/Users/Sulman/chromedriver.exe') 
   # use this if your env path of chromedriver is already setup
-  driver = Chrome()
+ # driver = Chrome()
   driver.implicitly_wait(10)
   yield driver
   driver.quit()
+  
